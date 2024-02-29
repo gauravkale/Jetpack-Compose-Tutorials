8d2854375bb62c4daa39bfcfa5fde2ff69eedc13
gauravkale <gauravrk29@gmail.com>
2024-02-29 20:59:54 +0530

    initial pending commit

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Jetpack-Compose-Tutorials"
+include(":Tutorial1-1Basics")
+include(":Tutorial2-1Unit-Testing")
+include(":Tutorial2-2UI-Testing")
